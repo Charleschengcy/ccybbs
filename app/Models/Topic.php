b0f1d83c8f1549477849db7bb3116896fa5dd264
Charlie <charleschengcy94@gmail.com>
2019-04-14 23:34:23 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,18 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+
+    /*category—— 一个话题属于一个分类；
+     user —— 一个话题拥有一个作者。*/
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,5 @@
+<div class="card ">
+  <div class="card-body">
+    Navigator
+  </div>
+</div>
