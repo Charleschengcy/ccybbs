63c815f66998157a21a6ed128360576bde1b3038
Charlie <charleschengcy94@gmail.com>
2019-04-16 19:47:15 +0000

    用户创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -10,17 +10,16 @@ class TopicRequest extends Request
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
+
                 ];
             }
             case 'GET':
@@ -35,7 +34,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
-            // Validation messages
+            'title.min' => 'Title contains two characters at least.',
+            'body.min' => 'Content contains three characters at least.',
         ];
     }
 }
