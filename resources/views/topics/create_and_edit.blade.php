2901406693d1784014439879cff06ccb1a34cae7
Charlie <charleschengcy94@gmail.com>
2019-04-17 00:00:04 +0000

    用户上传图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -71,6 +71,16 @@
     $(document).ready(function() {
       var editor = new Simditor({
         textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params: {
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount: 3,
+          leaveConfirm: 'File is uploading, are you sure to cancel it?'
+        },
+        pasteImage: true,
       });
     });
   </script>
