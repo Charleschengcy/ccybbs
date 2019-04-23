0581d28993e869ad93f4dcfef93677a95154600b
Charlie <charleschengcy94@gmail.com>
2019-04-23 20:11:43 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -70,6 +70,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
